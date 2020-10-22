9802bf6c2704e0dd8c1ec2e641164b05fcaa5584
Maxim Savateev <savateev@i-teco.ru>
2020-10-22 23:06:03 +0300

    Added build

---
+++ precache-manifest.65edcd0c47d2b221b280227ea50b0a28.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9747e2ee7b3f98a438339cc073fbfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d91afe47cc4e9b42a7",
+    "url": "/static/css/main.a014c08d.chunk.css"
+  },
+  {
+    "revision": "1f79aea07046c1a6da4c",
+    "url": "/static/js/2.82aea70b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.82aea70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d91afe47cc4e9b42a7",
+    "url": "/static/js/main.02f7472c.chunk.js"
+  },
+  {
+    "revision": "65bd36fa7cbb3c15f02e",
+    "url": "/static/js/runtime-main.3e3c6946.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
